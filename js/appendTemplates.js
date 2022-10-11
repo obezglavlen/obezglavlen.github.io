fcd4ffe98b0427ebb5f5dd530053568b2c5ef42e
obezglavlen <pcgalaxy123@gmail.com>
2022-10-11 12:40:05 +0300

    fix appenders

--- js/appendTemplates.js
+++ js/appendTemplates.js
@@ -1,9 +1,15 @@
-export async function appendNavigation() {
+export async function appendNavigation(location) {
   const html = await fetch("/templates/navigation.html").then((response) =>
     response.text()
   );
 
-  return html;
+  const nav = document.createElement("nav");
+  nav.innerHTML = html;
+  nav.className = "navigation";
+
+  nav.querySelector(`a[href="${location}"]`).setAttribute("active", "");
+
+  return nav;
 }
 
 export async function appendFooter() {
@@ -11,5 +17,9 @@ export async function appendFooter() {
     response.text()
   );
 
-  return html;
+  const footer = document.createElement("footer");
+  footer.innerHTML = html;
+  footer.className = "footer";
+
+  return footer;
 }
--- js/index.js
+++ js/index.js
@@ -2,14 +2,16 @@ import { appendNavigation, appendFooter } from "./appendTemplates.js";
 import { routes } from "./constants.js";
 
 const body = document.getElementsByTagName("body")[0];
+let location = `/${document.location.pathname.split("/")[1]}`;
+if (location === "/index.html") {
+  window.history.replaceState({}, "", "/");
+  location = "/";
+}
 
-appendNavigation().then((navigation) => {
-  body.innerHTML = navigation.concat(body.innerHTML);
-  const location = document.location.pathname.split("/")[1];
-
-  body.getElementsByTagName("a")[routes[location]].setAttribute("active", "");
+appendNavigation(location).then((navigation) => {
+  body.insertBefore(navigation, body.firstChild);
 });
 
 appendFooter().then((footer) => {
-  body.innerHTML = body.innerHTML.concat(footer);
+  body.appendChild(footer);
 });
