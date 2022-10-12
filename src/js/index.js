2a966bb7818404e17d15ed045b7cb8768e8fdacf
obezglavlen <pcgalaxy123@gmail.com>
2022-10-12 16:49:01 +0300

    upd path, styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import { appendNavigation, appendFooter } from "./appendTemplates.js";
 import { routes } from "./constants.js";
 
-const body = document.getElementsByTagName("body")[0];
+const body = document.querySelector("body");
 let location = `/${document.location.pathname.split("/")[1]}`;
 if (location === "/index.html") {
   window.history.replaceState({}, "", "/");
@@ -9,7 +9,7 @@ if (location === "/index.html") {
 }
 
 appendNavigation(location).then((navigation) => {
-  body.insertBefore(navigation, body.firstChild);
+  body.insertBefore(navigation, body.firstChild  );
 });
 
 appendFooter().then((footer) => {
