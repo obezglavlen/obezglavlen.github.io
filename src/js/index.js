05b5b5da81abd4e68c61e9a7bf3a32cc2845be76
obezglavlen <pcgalaxy123@gmail.com>
2022-10-13 19:05:30 +0300

    remove templates, change github link

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,6 @@
-import { appendNavigation, appendFooter } from "./appendTemplates.js";
-
-const body = document.querySelector("body");
-let location = `/${document.location.pathname.split("/")[1]}`;
-if (location === "/index.html") {
-  window.history.replaceState({}, "", "/");
-  location = "/";
-}
-
-appendNavigation(location).then((navigation) => {
-  body.insertBefore(navigation, body.firstChild  );
-});
-
-appendFooter().then((footer) => {
-  body.appendChild(footer);
-});
+(() => {
+  const location = `/${document.location.pathname.split("/")[1]}`;
+  if (location === "/index.html") {
+    window.history.replaceState({}, "", "/");
+  }
+})();
